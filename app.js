8123c85e2412f51493608eb9e1de5f138a0760f9
joannaberlin <joanna.kot.ok@gmail.com>
2023-10-09 12:16:13 +0200

    Add battle log functionality

--- app.js
+++ app.js
@@ -55,27 +55,39 @@ const app = Vue.createApp({
 			this.killaLove += kissValue;
 			this.kissBuddy();
 			alert('Killa kisses Buddy back!');
+			this.addLogMessage('killa', kissValue);
 		},
 		kissBuddy() {
 			const kissValue = getRandomValue(7, 15);
 			this.buddyLove += kissValue;
+			this.addLogMessage('buddy', kissValue);
 		},
 		kissRandom() {
-			const random = getRandomValue(1, 4);
+			const random = getRandomValue(1, 5);
 			if (random === 1) {
 				this.buddyLove += 10;
+				this.addLogMessage('buddy', 10);
 			} else if (random === 2) {
-				this.killaLove += 10;
+				this.killaLove += 15;
+				this.addLogMessage('killa', 15);
 			} else if (random === 3) {
 				this.buddyLove += 5;
+				this.addLogMessage('buddy', 5);
 			} else if (random === 4) {
-				this.killaLove += 5;
+				this.killaLove += 25;
+				this.addLogMessage('killa', 25);
 			}
 		},
 		surrenderToLove() {
 			this.killaLove = 100;
 			this.buddyLove = 100;
 			this.surrender = true;
+		},
+		addLogMessage(who, value) {
+			this.logMessages.unshift({
+				actionBy: who,
+				actionValue: value,
+			});
 		},
 	},
 });
